30220045eadb7cd979d2f5c3df8a324b388a128e
eckremer <70354307+eckremer@users.noreply.github.com>
2020-12-19 23:31:13 -0600

    Add files via upload

---
+++ ContactDriver.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author Erin Kremer
+ * The ContactDriver class is the class that calls the methods within Contact 
+ * and displays the information in it
+ *
+ */
+public class ContactDriver 
+{
+
+	public static void main(String[] args) 
+	{
+		Contact c = new Contact();
+		c.displayContactInformation();
+	}
+
+}
